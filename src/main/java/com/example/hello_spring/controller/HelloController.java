05a921b71320dd456cf16cd964507ffc16f0a680
jamin <cscsh2068@naver.com>
2025-11-01 18:47:34 +0900

    Add section1 practice code

---
+++ src/main/java/com/example/hello_spring/controller/HelloController.java
@@ -0,0 +1,47 @@
+package com.example.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello!!");
+        return "hello"; //resources 에 있는 hello.html로 가라
+    }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody //http body 부에 아래 내용을 직접 넣는다
+    public String HelloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody //
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
+
